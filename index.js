54237fc70740939a84c1e7ce8e9e2c7dfdbc0d2c
Hedayat Farahi <Hedayatullah.farahi@oerlikon.com>
2023-05-10 13:42:01 +0000

    fix the route

--- index.js
+++ index.js
@@ -27,27 +27,10 @@ app.get('/', (req, res) => {
 
 
 
-CategoryRoute = require('./routes/Category.route');
+CategoryRoute = require('./routes/Category.route.js');
 app.use('/category', CategoryRoute);
 
 
-
-
-
-// 404 Error handling
-app.use((req, res, next) => {
-    next(createError(404, "Not found"));
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-
-    res.send({
-        status: err.status || 500,
-        message: err.message,
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
