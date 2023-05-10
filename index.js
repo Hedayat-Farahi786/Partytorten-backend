f61b1cc02ca925ea0c766ab228fe3b6cb447ba24
Hedayat Farahi <Hedayatullah.farahi@oerlikon.com>
2023-05-10 13:43:47 +0000

    fix the bug

--- index.js
+++ index.js
@@ -27,8 +27,32 @@ app.get('/', (req, res) => {
 
 
 
-CategoryRoute = require('./routes/Category.route.js');
-app.use('/category', CategoryRoute);
+// CategoryRoute = require('./routes/Category.route.js');
+// app.use('/category', CategoryRoute);
+
+
+
+app.get("/", async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.json(categories);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred' });
+    }
+});
+
+app.post("/", async (req, res) => {
+    try {
+        const { name } = req.body;
+        const category = new Category({ name });
+        await category.save();
+        res.json(category);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An error occurred' });
+    }
+});
 
 
 
