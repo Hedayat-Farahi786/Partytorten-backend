26b10982a26d1648d6a69c423dc986d099f5364c
Hedayat Farahi <hedayatfarahi8@gmail.com>
2023-05-24 22:15:57 +0200

    login user after signup

--- routes/Signup.route.js
+++ routes/Signup.route.js
@@ -13,8 +13,8 @@ router.post("/", async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         const result = await user.save();
-        // const token = jwt.sign({ userId: user._id, name, email }, process.env.JWT_SECRET);
-        res.json({ result });
+        const token = jwt.sign({ userId: user._id, email: user.email, name: user.name }, process.env.JWT_SECRET);
+        res.json({ token });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'An error occurred' });
